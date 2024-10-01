0edad4810bc318e5096928770475457efee6480f
Admin <Admin@MT0430>
2024-10-01 09:39:00 +0700

    singleton done

---
+++ ConsoleApp1/Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Adapter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create adapter and place a request
+            Target target = new Adapter();
+            target.Request();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
