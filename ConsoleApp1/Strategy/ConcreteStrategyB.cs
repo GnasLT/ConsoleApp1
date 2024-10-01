9ee07ebee18733920abd07f01357b8f46d7ff0db
Admin <Admin@MT0430>
2024-10-01 09:43:23 +0700

    Strategy done

---
+++ ConsoleApp1/Strategy/ConcreteStrategyB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Strategy
+{
+    public class ConcreteStrategyB : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine(
+                "Called ConcreteStrategyB.AlgorithmInterface()");
+        }
+    }
+
+}
---
+++ ConsoleApp1/Strategy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Strategy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Context context;
+            // Three contexts following different strategies
+            context = new Context(new ConcreteStrategyA());
+            context.ContextInterface();
+            context = new Context(new ConcreteStrategyB());
+            context.ContextInterface();
+            context = new Context(new ConcreteStrategyC());
+            context.ContextInterface();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
